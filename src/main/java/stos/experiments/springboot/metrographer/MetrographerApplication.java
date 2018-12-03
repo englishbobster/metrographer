309124528e2da7a08a8fccccf51e967c2e92ba4f
Stuart Osborne <stuart.osborne@infinera.com>
2018-12-03 21:32:40 +0100

    Initial commit.

---
+++ src/main/java/stos/experiments/springboot/metrographer/MetrographerApplication.java
@@ -0,0 +1,12 @@
+package stos.experiments.springboot.metrographer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrographerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrographerApplication.class, args);
+	}
+}
